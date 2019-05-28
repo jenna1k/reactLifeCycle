ecbe9df9aa80f20005ec74b0d6cbc13d20f99547
jenna <jenna_kim1@hotmail.com>
2019-05-28 13:32:11 +0200

    finished react life cycle quest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import "./App.css";
-import Clock from './Clock'
-import Next from './Next'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = { 
+      title: 'put some words here!',
+      globalTitle: 'change the title!'
+    };
+
+    this.clickButton = this.clickButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  clickButton(){
+    document.title = this.state.globalTitle
+  }
+
+  handleChange(e){
+    this.setState({
+      globalTitle: e.target.value
+    })
+  }
+
+  componentDidMount(){
+    console.log('Rendered Form')
+  }
+
+  componentDidUpdate(){
+    console.log('Title changed')
+  }
+
   render() {
         return (
-          <BrowserRouter>
-            <div>
-              <NavLink exact to="/" activeClassName="selected" activeStyle={{color: "red"}}> Home </NavLink>
-              <NavLink to="/Next" activeClassName="selected" activeStyle={{color: "red"}}> Next </NavLink>
-
-              <Switch>
-                <Route exact path="/" component={Clock} />
-                <Route path="/Next" component={Next} />
-              </Switch>
-            </div>
-        </BrowserRouter>
+          <div>
+            <h1>{this.state.globalTitle}</h1>
+            <form>
+              <input type="text" name="text" placeholder={this.state.title} onChange={this.handleChange}/>
+              <div>
+                <p>if you want to change webpage title</p>
+              </div>
+              <button onClick={(e) => {
+                e.preventDefault();
+                this.clickButton();
+              }}>click here</button>
+            </form>
+          </div>
         );
     }
 }
